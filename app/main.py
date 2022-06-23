2d6efabc1650461dd0505be89055ccd2ddf3fd03
kilian <21954712+KilianZimmerer@users.noreply.github.com>
2022-06-23 14:06:11 +0200

    first working draft

---
+++ app/main.py
@@ -0,0 +1,15 @@
+from fastapi import FastAPI
+
+from app.api.api import router as api_router
+from mangum import Mangum
+
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    return {"message`": "Hello World!"}
+
+
+app.include_router(api_router, prefix="/api")
+handler = Mangum(app)
