93e0342f41a594fb3bd204254bcd26ffc8e1e79e
kilian <21954712+KilianZimmerer@users.noreply.github.com>
2022-06-27 15:14:13 +0200

    add credentials file to gitifnore and rename endpoint

--- src/api/api.py
+++ src/api/api.py
@@ -4,4 +4,4 @@ from src.api.endpoints import integrate_products
 
 
 router = APIRouter()
-router.include_router(integrate_products.router, prefix="/transform", tags=["Transform"])
+router.include_router(integrate_products.router, prefix="/integrate_products", tags=["Product Integration"])
