5d47a61b41edbc2bee40427dfc66520c2661a73f
kilian <21954712+KilianZimmerer@users.noreply.github.com>
2022-06-27 11:45:23 +0200

    fix method name

--- src/api/api.py
+++ src/api/api.py
@@ -4,4 +4,4 @@ from src.api.endpoints import integrate_products
 
 
 router = APIRouter()
-router.include_router(transform.router, prefix="/transform", tags=["Transform"])
+router.include_router(integrate_products.router, prefix="/transform", tags=["Transform"])
